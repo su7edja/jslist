0bed3c17e6e0c69d094ce43da67bf0152a0352c3
su7edja <su7edja@gmail.com>
2018-03-24 18:58:17 -0700

    Basic functionality for jslist done

--- list.js
+++ list.js
@@ -1,9 +1,17 @@
-function strike() {
-	console.log("strike");
-}
-
-function remove() {
+function addXButton(e) {
 	console.log("remove");
+	var xButton = document.createElement("span");
+	var xButtonValue = document.createTextNode("\u00D7");
+	xButton.appendChild(xButtonValue);
+	xButton.addEventListener(
+		"click",
+	 	function (ev) {
+	 		console.log(ev.target.parentElement.tagName);
+	 		ev.target.parentElement.style.display = "none";
+	 	},
+	 	false);
+	xButton.classList.add("close");
+	e.appendChild(xButton);
 }
 
 function addNew() {
@@ -24,9 +32,23 @@ function addNew() {
 	document.getElementById("new").value = "";
 
 	// Add cross button to remove from list
-	remove();
-
-	// Add strike capability
-	strike();
+	addXButton(newLi);
 
 }
+
+console.log("strike");
+// Get ul element, attach eventListener for every li in it
+var l = document.getElementById("uList");
+l.addEventListener(
+	"click",
+	function (ev){
+		if(ev.target.tagName == "LI"){
+			ev.target.classList.toggle("strike");
+		}
+	},
+	false);
+
+var items = l.getElementsByTagName("li");
+for (var i = 0; i < items.length; ++i) {
+  addXButton(items[i]);
+}	
