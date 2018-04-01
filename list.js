841c6bddfa48ea93ed1e28782ffd8446c2c4f15c
su7edja <su7edja@gmail.com>
2018-04-01 14:05:36 -0700

    Added filter capability, moved adding new things to bottom, with multiple fields. To do: incorporate additional fields to list, stylized the add table and make table's display none on first load

--- list.js
+++ list.js
@@ -7,33 +7,64 @@ function addXButton(e) {
 		"click",
 	 	function (ev) {
 	 		console.log(ev.target.parentElement.tagName);
-	 		ev.target.parentElement.style.display = "none";
+	 		// ev.target.parentElement.style.display = "none";
+	 		var ul = ev.target.parentElement.parentElement;
+	 		var li = ev.target.parentElement;
+	 		ul.removeChild(li);
 	 	},
 	 	false);
 	xButton.classList.add("close");
 	e.appendChild(xButton);
 }
 
-function addNew() {
-	console.log("addNew!!!");
-	
-	// Create li element, get input value, check for empty string, clear out input value
+function appendToList(item) {
 	var newLi = document.createElement("li");
-	var inputValue = document.getElementById("new").value;
-	console.log(inputValue);
-	var livalue = document.createTextNode(inputValue);
+	var livalue = document.createTextNode(item);
 	newLi.appendChild(livalue);
 	var l = document.getElementById("uList");
-	if (inputValue == "") {
+	if (item == "") {
 		alert ("Put something, yo!");
 	} else {
+		// Add cross button to remove from list
+		addXButton(newLi);
 		l.appendChild(newLi);
 	}
-	document.getElementById("new").value = "";
+}
 
-	// Add cross button to remove from list
-	addXButton(newLi);
+function showAddNew() {
+	console.log("showAddNew");
+	var addNewTable = document.getElementById("newTable");
+	if (addNewTable.style.display == "none") {
+		addNewTable.style.display = "";
+	}
+	else {
+		addNewTable.style.display = "none";
+	}
+}
 
+function parseForm() {
+	console.log("parseForm");
+	var todo = document.getElementById("todo").value || "";
+	var state = document.getElementById("state").value;
+	var rdate = document.getElementById("rdate").value;
+	console.log(todo + ", " + state + ", " + rdate);
+
+	appendToList(todo);
+}
+
+function filterList() {
+	var filter = document.getElementById('filterBar').value.toLowerCase();
+	console.log("filterList: " + filter);
+	var lis = document.getElementById("uList").getElementsByTagName("li");
+	for (var i = 0; i < lis.length; i++) {
+		console.log(lis[i].innerText.toLowerCase().slice(0,-1));
+		if (lis[i].innerText.toLowerCase().slice(0,-1).indexOf(filter) > -1) {
+			lis[i].style.display = "";
+		}
+		else {
+			lis[i].style.display = "none";
+		}
+	}
 }
 
 console.log("strike");
@@ -52,3 +83,4 @@ var items = l.getElementsByTagName("li");
 for (var i = 0; i < items.length; ++i) {
   addXButton(items[i]);
 }	
+
